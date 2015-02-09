8fd163bf116f8733a305c6e637a90f8da6629b20
Sam Kozin <i.a.m@bk.ru>
2015-02-09 23:13:44 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var log = makeLogWithName(undefined);
+log.for = makeLogWithName;
+
+
+function makeLogWithName(name) {
+  var log = makeLogFunc(name);
+      log.TODO = makeLogFunc(name, 'TODO');
+      log.info = makeLogFunc(name, 'INFO');
+      log.warn = makeLogFunc(name, 'WARN');
+      log.error = makeLogFunc(name, 'ERROR');
+  return log;
+}
+
+
+function makeLogFunc(name, type) {
+  if (name == undefined && type == undefined) {
+    return function() {
+      console.error.apply(console, arguments);
+    };
+  } else {
+    var header = (name == undefined)
+      ? '{' + type + '}'
+      : (type == undefined)
+        ? '[' + name + ']'
+        : '[' + name + '] {' + type + '}';
+    return function() {
+      var args = arguments.length == 1 ? [arguments[0]] : Array.apply(null, arguments);
+      args.unshift(header);
+      console.error.apply(console, args);
+    };
+  }
+}
+
+
+module.exports = log;
